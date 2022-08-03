55fc6860435db731e171d573f64c68083920633e
Falendrac <leger_franck@hotmail.fr>
2022-08-03 14:31:33 +0200

    Improve method by using recursion

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,25 +5,28 @@ Can we unlock the lockboxes ?
 '''
 
 
+def searchKey(boxes, key, boxesOpen):
+    '''
+    Open all boxes and store all key
+    '''
+    allKeys = key
+    for element in key:
+        if (element < len(boxes) and element not in boxesOpen):
+            boxesOpen.append(element)
+            allKeys = allKeys + searchKey(boxes, boxes[element], boxesOpen)
+
+    return allKeys
+
+
 def canUnlockAll(boxes):
     '''
     Determines if all the boxes can be opened.
     '''
-    listKey = boxes[0]
-
-    for element in boxes[0]:
-        if (element < len(boxes)):
-            listKey = listKey + boxes[element]
+    boxesOpen = [0]
+    listKey = searchKey(boxes, boxes[0], boxesOpen)
 
     for idx in range(1, len(boxes)):
-        canOpen = False
-        for key in listKey:
-            if (key == idx):
-                canOpen = True
-
-        if (canOpen):
-            listKey = listKey + boxes[idx]
-        else:
+        if (idx not in listKey):
             return False
 
     return True
