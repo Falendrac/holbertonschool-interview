c7b2986f1a01f9ef5e614dbcb67d4fe1b165921d
Falendrac <leger_franck@hotmail.fr>
2022-08-18 12:17:14 +0200

    Add task 0 to 0x02.

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ *
+ * @parent: The previous binary tree node
+ * @value: Value set in the new node
+ * Return: New node or NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
