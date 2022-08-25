cd1dc0646e29ce56b155c85ebb2a343ea1c0708a
Falendrac <leger_franck@hotmail.fr>
2022-08-25 15:33:59 +0200

    Add task

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Interview we search the minimal operations
+'''
+
+def minOperations(n):
+    '''
+    calculates the fewest number of operations needed to result
+    in exactly n H characters in the file.
+    '''
+    if type(n) is not int or n <= 1:
+        return 0
+
+    coef = 2
+    result = 0
+
+    while coef <= n:
+        if n % coef == 0:
+            n = n / coef
+            result += coef
+        else:
+            coef += 1
+
+    return result
